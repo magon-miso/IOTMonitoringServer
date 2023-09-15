5e4fc46ff5a00db2c3f8f8ecb6441ef40b9ff13e
magon-miso <m.agonf@uniandes.edu.co>
2023-09-15 16:34:27 -0500

    change settings ip servers

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.86.227.50"]
+ALLOWED_HOSTS = ["localhost", "54.162.255.63"]
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'crispy_bootstrap4',
     'control',
     'receiver',
     'viewer'
@@ -95,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.80.194.130",  # Dirección IP de la base de datos
+        "HOST": "44.208.33.121",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "35.175.144.204"
+MQTT_HOST = "35.173.198.60"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
