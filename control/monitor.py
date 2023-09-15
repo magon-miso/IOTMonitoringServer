199af944a758bf90cf9b78fd93f71327586461d3
magon-miso <m.agonf@uniandes.edu.co>
2023-09-15 18:31:15 -0500

    analyze_data check logs

--- control/monitor.py
+++ control/monitor.py
@@ -45,13 +45,14 @@ def analyze_data():
         city = item['station__location__city__name']
         user = item['station__user__username']
 
+        print(datetime.now(), "check_value {} {}".format(variable, item["check_value"]))
         if item["check_value"] > max_value or item["check_value"] < min_value:
             alert = True
 
         if alert:
             message = "ALERT {} {} {}".format(variable, min_value, max_value)
             topic = '{}/{}/{}/{}/in'.format(country, state, city, user)
-            print(datetime.now(), "Sending alert to {} {}".format(topic, variable))
+            print(datetime.now(), "Sending alert to {} {}".format(topic, variable), message)
             client.publish(topic, message)
             alerts += 1
 
